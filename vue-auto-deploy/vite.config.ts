749b501b6cc086f6eac29f34204eed0cbbc8ca92
Pablo <pcs58@alu.ua.es>
2025-10-25 18:53:57 +0200

    Configura base para GitHub Pages

--- vue-auto-deploy/vite.config.ts
+++ vue-auto-deploy/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/web_train/', // 👈 muy importante: nombre exacto del repo
   plugins: [
     vue(),
     vueDevTools(),
